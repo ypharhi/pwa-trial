281565d975f02a7dc69e2724445872af1a76dffe
ypharhi <yaron.pharhi@gmail.com>
2023-09-07 17:46:24 +0300

    add first pwa example (using vs code)

---
+++ readme.txt
@@ -0,0 +1,25 @@
+## Running the PWA Project Locally
+
+### Install Node.js and npm
+- Download and install Node.js and npm from the [official website](https://nodejs.org/).
+
+### Verify Installation
+Check Node.js and npm versions to verify installation:
+node -v
+npm -v
+ 
+### Install http-server
+Install `http-server` globally to run your project locally:
+ npm install -g http-server
+ 
+### Start the Project
+Navigate to your project directory and run the http-server:
+ cd path/to/project/directory
+http-server
+ Or use `npx`:
+ npx http-server - this works!
+ 
+### Access the Project
+Open a web browser and navigate to:
+ http://127.0.0.1:8080
+ 
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'my-first-pwa-cache-v1';
+const FILES_TO_CACHE = [
+  'index.html',
+  'styles.css',
+  'script.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
